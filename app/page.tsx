536ce323786f1d6a1d59f8e622c954adaadaa30c
Evlona <evlona@ops.io>
2024-07-17 02:59:20 +0300

    test1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>
-    Github Pages
-  </main>
+  return (
+    <main className={styles.main}>
+      Github Actions
+      Github Pages
+    </main>
+  );
 }
